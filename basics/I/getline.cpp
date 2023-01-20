df9105601f6edd5219e310accb78ecf3eb5bc20b
Clement Owireku-Bogyah <zenerbogyah7@gmail.com>
2023-01-20 15:59:39 +0000

    changes

--- basics/I/getline.cpp
+++ basics/I/getline.cpp
@@ -1,27 +1,23 @@
 #include <iostream>
 #include <string>
-#include <typeinfo>
 
 using namespace std;
 
 int main(void)
 {
-    // string line;
-    // //this is an auto type specifier
-    // while(getline(cin, line))
-    // {
-    //     if(!line.empty() && line.size() > 3){
-    //         cout << line << endl;
-    //     }else{
+    string line;
+    //this is an auto type specifier
+    while(getline(cin, line))
+    {
+        if(!line.empty() && line.size() > 3){
+            cout << line << endl;
+        }else{
 
-    //     cout<<"Empty line or less than 3"<<endl;
-    //     }
-    //     break;
-    // }
-    char b = 'a', c = 'x';
-    auto sum = c + b;
-
-    cout<< typeid(sum).name()<<endl;
+        cout<<"Empty line or less than 3"<<endl;
+        }
+        break;
+    }
+    
 
 
     
