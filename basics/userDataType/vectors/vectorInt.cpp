babe68d3a24b74f4412043487d51b779e160691c
Clement Owireku-Bogyah <zenerbogyah7@gmail.com>
2023-01-24 19:48:14 +0000

    files

--- basics/userDataType/vectors/vectorInt.cpp
+++ basics/userDataType/vectors/vectorInt.cpp
@@ -10,12 +10,29 @@ int main(void)
 {
     //declaring a vector
     vector <int> ivec;
+  
+    // for (int i = 0; i <100;++i)
+    // {
+    //     ivec.push_back(i);
+    //     cout<<ivec.at(i)<<endl;
+    // }
 
-    for(int i = 0; i < 100; ++i)
-        ivec.push_back(i);
-        
+    vector<string> svec;
+    string words;
+    cout<<"Enter letter or stop to terminate"<<endl;
+    while (cin >> words)
+    {
+        if (words == "stop")
+            break;
+        else
+            svec.push_back(words);
+    };
+    for(int i = 0; i < svec.size(); i++)
+        cout<<svec.at(i)<<endl;
+ 
 
     
-
+   
+        
     return 0;
 }
