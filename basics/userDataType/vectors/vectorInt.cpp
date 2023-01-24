b12d12b4852476c245044746deba6fa572b86b03
Clement Owireku-Bogyah <zenerbogyah7@gmail.com>
2023-01-24 16:30:18 +0000

    files

---
+++ basics/userDataType/vectors/vectorInt.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+/**
+ * vector uses vector header.
+*/
+
+int main(void)
+{
+    //declaring a vector
+    vector <int> ivec;
+
+    for(int i = 0; i < 100; ++i)
+        ivec.push_back(i);
+        
+
+    
+
+    return 0;
+}
