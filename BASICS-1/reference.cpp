ed7dbe4343d3c9761c873a1a069e566a7874910d
Clement Owireku-Bogyah <zenerbogyah7@gmail.com>
2023-01-31 23:44:30 +0000

    files

--- BASICS-1/reference.cpp
+++ BASICS-1/reference.cpp
@@ -5,12 +5,33 @@ using namespace std;
 /**
  * @d - reference type
 */
+int add(int x,int y);
 
 int main(void)
 {
-    int myAge = 17;
-    int &refAge = myAge;
+    //using the auto type and reference
+    auto myAge = 17;
+    auto &refAge = myAge;
     
+    struct newName
+    {
+        char name[20];
+    };
+    
+    //alias ..can use typedef too
+    using NA = struct newName;
+
+    NA clement = {"clement"};
+    cout<<clement.name<<endl;
+
+    // add(2,3)
+    decltype (add(2,3)) sum = 5;
+    cout<<sum<<endl;
+
     cout<<refAge/2<<endl;
     return 0;
+}
+int add(int x,int y)
+{
+    return x+y;
 }
