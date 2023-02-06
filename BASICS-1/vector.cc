ba1cc8bf8822731f5434786eb69ed7fc17a1abdd
Clement Owireku-Bogyah <zenerbogyah7@gmail.com>
2023-02-06 21:09:48 +0000

    file

--- BASICS-1/vector.cc
+++ BASICS-1/vector.cc
@@ -6,17 +6,31 @@ using namespace std;
 
 int main(void)
 {
-    vector<string> name;
-    string word;
-
-    while (cin >> word)
-    {
-        name.push_back(word);
-        break;
-    }
+    string name("something");
     
-    auto first = name.begin();
+    for(auto it = name.begin();it != name.end() && !isspace(*it);++it)
+    {
+        *it = toupper(*it);
+        cout << *it <<endl;
+    }
 
-    cout << *first <<endl;
+    /**
+    // iterator types
+    vector<int> :: iterator it1; //read and write
+    string::iterator it2; // read and write
+    vector<int>::const_iterator it3; //read only
+    string::const_iterator it4; //read only
+    */
+
+   const vector<int> ivec ;
+   ivec = {1,2,3,4};
+    // for(int i = 0; i != 10; ++i)
+    // {
+    //     ivec.push_back(i);
+    // }
+   auto ci = ivec.cbegin(); //const_iterator type
+   cout << *ci << endl;
+
+
     return 0;
 }
